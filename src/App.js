3e02ceb83ea69e050babe512e5c476a150f6da3a
matt <echoedinvoker@gmail.com>
2023-11-19 10:38:07 +0800

    q

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import styled, { keyframes } from "styled-components";
+
+import Slider from 'react-slick';
+
+const Box = styled.div`
+  width: 10rem
+`
 
 function App() {
+  const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div style={{ background: '#545454'}}>
+        <h2>Single Item</h2>
+        <Slider {...settings}>
+          <Box>
+            <h3>2</h3>
+          </Box>
+        </Slider>
+      </div>
+    
   );
 }
 
